52852eec317baad01ef40d614496703b2d819283
IntegerValue <grummflash@mail.ru>
2024-07-16 12:11:25 +0700

    Create Project

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
-import os
 import flet as ft
-
+import os
 
 def create_section(title: str, file_name: str):
-    """
-    Создает раздел книги с заданным названием и содержимым из файла.
-
-    Args:
-        title (str): Название раздела.
-        file_name (str): Имя файла в папке "Book".
-    """
-    book_dir = os.path.join(os.path.dirname(__file__), "Book")
-    file_path = os.path.join(book_dir, f"{file_name}.txt")
+    file_path = os.path.join(os.path.dirname(__file__), title, f"{file_name}.txt")
 
     if os.path.exists(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
             content = file.read()
 
-        section = ft.Column(
+        return ft.Column(
             controls=[
                 ft.Text(title, size=24, weight="bold"),
                 ft.Divider(),
-                ft.Markdown(content)
+                #ft.Markdown(content)
             ]
         )
-        return section
     else:
         return ft.Text("Ошибка - файл не найден", color="red")
-
 
 def main(page: ft.Page):
     page.title = "Uncharted Sector"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    section = create_section("Введение", "Введение")
-    page.add(section)
+    page.add(create_section("Введение", "Введение"))
+    page.scroll = "AUTO"
+    page.update()
 
 
 ft.app(target=main)
