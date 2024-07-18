0e4c5f5df063941422a22ee09c638d5311b45336
IntegerValue <grummflash@mail.ru>
2024-07-18 14:37:38 +0700

    Create Project

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
 import flet as ft
-import os
-import time
-
-
-def edit_typewriter(r, d_time=0.1, text=""):
-
-    while r.value != text:
-        past_text = r.value
-        for char in past_text:
-            if char != " ":
-                r.value = r.value.replace(char, "  ", 1)
-                r.page.update()
-                time.sleep(0.1)
-            else:
-                break
-        for char in r.value:
-            if char == " ":
-                r.value = r.value[1:]
-                time.sleep(0.01)
-            else:
-                break
-
-
-
-def create_section(title: str, file_name: str):
-    file_path = os.path.join(os.path.dirname(__file__), "Resources", title, f"{file_name}.md")
-    if os.path.exists(file_path):
-        with open(file_path, "r", encoding="utf-8") as file:
-            content = file.read()
-
-        return ft.Markdown(content, selectable=True)
-    else:
-        return ft.Text("Ошибка - файл не найден", color="red")
+from screeninfo import get_monitors
 
 
 def main(page: ft.Page):
-    page.title = "Uncharted Sector"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    #---Настройки окна---
+    page.title = "Uncharted Sector Main Book"
+    page.theme_mode = 'dark'
+    page.window.width = get_monitors()[0].width / 1.25
+    page.window.height = get_monitors()[0].height / 1.35
+    page.window.alignment = ft.Alignment(0, 0)
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.scroll = True
 
-    text = create_section("Введение", "Введение")
-    page.add(text)
-    page.scroll = "AUTO"
-
-    file_path = os.path.join(os.path.dirname(__file__), "Resources", "Введение", f"{Введение_без_воды}.md")
-    with open(file_path, "r", encoding="utf-8") as file:
-        content = file.read()
-
-    button = ft.ElevatedButton("Запустить анимацию", on_click=lambda _: edit_typewriter(text, d_time=2, text=content))
-    button.bgcolor = "blue"
-    button.color = "white"
-    button.alignment = ft.MainAxisAlignment.START
-    button.z_index = 1
-    page.add(button)
-    page.update()
+    border = ft.Border(top=ft.BorderSide(width=1, color='#171a1a'),
+                       bottom=ft.BorderSide(width=1, color='#171a1a'),
+                       left=ft.BorderSide(width=1, color='#171a1a'),
+                       right=ft.BorderSide(width=1, color='#171a1a')
+                       )
+    page.add(
+        ft.Row(
+            [
+                ft.Container(
+                    padding=10,
+                    border=border,
+                    bgcolor='#426b6b',
+                    border_radius=10,
+                    content=ft.Column(
+                        [
+                            ft.Text("Uncharted Sector"),
+                            ft.ElevatedButton("Лист персонажа"),
+                            ft.ElevatedButton("Правила"),
+                            ft.ElevatedButton("Бестиарий"),
+                            ft.Row([ft.IconButton(icon=ft.icons.SETTINGS), ft.IconButton(icon=ft.icons.TURNED_IN)],
+                                   alignment=ft.MainAxisAlignment.SPACE_AROUND)
+                        ], alignment=ft.MainAxisAlignment.CENTER
+                    )
+                )
+            ]
+        )
+    )
 
 
 ft.app(target=main)
