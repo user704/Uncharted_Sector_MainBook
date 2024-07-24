9a4a8bc996c422ce6e1c58c83ceadf04e15b5d23
IntegerValue <grummflash@mail.ru>
2024-07-24 13:24:27 +0700

    Работа над анимированными переходами

--- C_UI/main_menu.py
+++ C_UI/main_menu.py
@@ -1,27 +1,30 @@
 import flet as ft
+import time
 
 
 class MainMenu:
-    def __init__(self, page: ft.Page):
+    #---Выполняется при создании класса---
+    def __init__(self, page: ft.Page, is_anim_from_start=True):
         self.page = page
+        self.page.title = "Uncharted Sector: Main Menu"
         self.del_before()
-        self.create_widgets()
+        self.anim_start(is_anim_from_start)
 
+    # ---Перед появлением все предыдущие меню стираются---
     def del_before(self):
         self.page.clean()
 
+    #---Кнопка Выход---
     def exit_app(self, e):
         self.page.window.close()
 
+    #---Главное меню --> Настройки---
     def to_settings(self, e):
         from C_UI.settings import Settings
         Settings(self.page)
 
-    def create_widgets(self):
-        self.page.title = "Uncharted Sector: Main Menu"
-        b_style = ft.BorderSide(width=1, color='cyan')
-        border = ft.Border(top=b_style, bottom=b_style, left=b_style, right=b_style)
-
+    #---Создание кнопок и основного функционала---
+    def create_btns(self):
         #---Заголовок---
         title_t = ft.Column([
             ft.Text(value="Uncharted Sector",
@@ -46,20 +49,77 @@ class MainMenu:
             ft.OutlinedButton(text="Выйти", scale=1.2, width=170, on_click=self.exit_app)
         ], horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             alignment=ft.MainAxisAlignment.SPACE_AROUND,
-        height=230)
+            height=230)
 
-        #---Разметка---
-        layout = ft.Column([title_t, menu_btns],
-                           horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+        #---Вывод как переменная---
+        return ft.Column([title_t, menu_btns], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
 
-        #---Контейнер---
-        menu_ctr = ft.Container()
-        menu_ctr.padding = 10
-        menu_ctr.margin = 0
-        menu_ctr.border = border
-        menu_ctr.border_radius = 10
-        menu_ctr.height = 350
-        menu_ctr.width = 300
-        menu_ctr.content = layout
+    # ---Спавн главного меню с анимацией---
+    def anim_start(self, from_start=True):
+        # ---Создание контейнера для дальнейших анимаций---
+        def create_container():
+            b_style = ft.BorderSide(width=1, color='cyan')
+            border = ft.Border(top=b_style, bottom=b_style, left=b_style, right=b_style)
 
-        self.page.add(menu_ctr)
+            return ft.Container(padding=10, margin=0, border=border)
+
+        # ---Анимация если меню вызвано с нуля---
+        def anim_from_start():
+            # ---Начальные параметры контейнера---
+            container = create_container()
+            container.border_radius = 40
+            container.height = 40
+            container.width = 40
+            container.animate_opacity = ft.animation.Animation(500, curve=ft.AnimationCurve.EASE)
+            container.opacity = 0
+            container.animate = ft.animation.Animation(300, curve=ft.AnimationCurve.EASE)
+            self.page.add(container)
+
+            # ---Появление---
+            time.sleep(0.3)
+            container.opacity = 1
+            self.page.update()
+
+            # ---Анимация ширины---
+            time.sleep(0.5)
+            container.width = 300
+            container.border_radius = 10
+            self.page.update()
+
+            # ---Анимация высоты---
+            time.sleep(0.3)
+            container.height = 350
+            self.page.update()
+
+            # ---Анимация появления кнопок---
+            btns = self.create_btns()
+            btns.animate_opacity = ft.animation.Animation(500, curve=ft.AnimationCurve.EASE)
+            btns.opacity = 0
+            container.content = btns
+            self.page.update()
+            time.sleep(0.2)
+            btns.opacity = 1
+            self.page.update()
+
+        def anim_not_from_start():
+            # ---Начальные параметры контейнера---
+            container = create_container()
+            container.border_radius = 10
+            container.height = 350
+            container.width = 300
+            self.page.add(container)
+
+            # ---Анимация появления кнопок---
+            btns = self.create_btns()
+            btns.animate_opacity = ft.animation.Animation(500, curve=ft.AnimationCurve.EASE)
+            btns.opacity = 0
+            container.content = btns
+            self.page.update()
+            time.sleep(0.2)
+            btns.opacity = 1
+            self.page.update()
+
+        if from_start:
+            anim_from_start()
+        else:
+            anim_not_from_start()
--- C_UI/settings.py
+++ C_UI/settings.py
@@ -1,4 +1,5 @@
 import flet as ft
+import time
 
 
 class Settings:
@@ -6,22 +7,25 @@ class Settings:
     # ---Инициализация класса---
     def __init__(self, page: ft.Page):
         self.page = page
+        self.page.title = "Uncharted Sector: Settings"
         self.del_before()
-        self.create_widgets()
+        self.anim_start()
 
     #---Удаление предыдущего окна---
     def del_before(self):
         self.page.clean()
 
     #---Сохранение настройки в файл---
-    def save_to_file(self, key, value):
+    @staticmethod
+    def save_to_file(key, value):
+        #Поиск абсолютного пути
         from os import path
         file_path = path.abspath(path.dirname(__file__))
         file_path = path.join(file_path, '..', "Data", "settings.json")
-
+        #Перезаписываем файл настроек
         import json
         with open(file_path, 'w', encoding='UTF-8') as file:
-            json.dump({key : value}, file)
+            json.dump({key: value}, file)
 
     #---Переключение темы---
     def change_theme(self, e):
@@ -30,21 +34,60 @@ class Settings:
         else:
             self.page.theme_mode = 'light'
         self.page.update()
-        self.save_to_file('theme_mode', value= self.page.theme_mode)
+        self.save_to_file('theme_mode', value=self.page.theme_mode)
 
     #---Настройки --> Главное меню---
     def back_to_menu(self, e):
         from C_UI.main_menu import MainMenu
         MainMenu(self.page)
 
+    # ---Спавн меню настроек с анимацией---
+    def anim_start(self):
+        # ---Создание контейнера для дальнейших анимаций---
+        def create_container():
+            b_style = ft.BorderSide(width=1, color='cyan')
+            border = ft.Border(top=b_style, bottom=b_style, left=b_style, right=b_style)
+
+            return ft.Container(padding=10, margin=0, border=border)
+
+        # ---Начальные параметры контейнера---
+        container = create_container()
+        container.border_radius = 10
+        container.height = 350
+        container.width = 300
+        container.animate = ft.animation.Animation(300, curve=ft.AnimationCurve.EASE)
+        self.page.add(container)
+
+        # ---Анимация ширины---
+        time.sleep(0.5)
+        container.width = 300
+        self.page.update()
+
+        # ---Анимация высоты---
+        time.sleep(0.3)
+        container.height = 350
+        self.page.update()
+
+        # ---Анимация появления кнопок---
+        btns = self.create_widgets()
+        btns.animate_opacity = ft.animation.Animation(500, curve=ft.AnimationCurve.EASE)
+        btns.opacity = 0
+        container.content = btns
+        self.page.update()
+        time.sleep(0.2)
+        btns.opacity = 1
+        self.page.update()
+
     # ---Создание виджетов---
     def create_widgets(self):
 
+        #---Заголовок раздела настроек---
         def section_title(title=''):
-            row = ft.Row([ft.Text(title)], alignment=ft.MainAxisAlignment.CENTER)
+            row = ft.Row([ft.Text(title, size=20, weight=ft.FontWeight('bold'))], alignment=ft.MainAxisAlignment.CENTER)
 
-            return [ft.Divider(), row, ft.Divider()]
+            return [ft.Divider(thickness=2), row, ft.Divider(thickness=2)]
 
+        #---Пункт настроек - тумблер---
         def settings_switch(name='', current_value=False, event=None):
             text = ft.Text(name, scale=1.2, text_align=ft.TextAlign.CENTER, offset=ft.Offset(x=0.15, y=0))
             switch = ft.Switch(value=current_value, on_change=event)
@@ -52,10 +95,7 @@ class Settings:
 
             return [row, ft.Divider()]
 
-        self.page.title = "Uncharted Sector: Settings"
-        b_style = ft.BorderSide(width=1, color='cyan')
-        border = ft.Border(top=b_style, bottom=b_style, left=b_style, right=b_style)
-
+        #---Список настроек---
         settings_list = ft.ListView(
             section_title('Визуальные настройки') +
             settings_switch(name="Тёмная / Светлая тема",
@@ -72,15 +112,4 @@ class Settings:
                               on_click=self.back_to_menu)
         ], alignment=ft.MainAxisAlignment.SPACE_AROUND)
 
-        #---Контейнер---
-        menu_ctr = ft.Container()
-        menu_ctr.padding = 10
-        menu_ctr.margin = 0
-        menu_ctr.border = border
-        menu_ctr.border_radius = 10
-        menu_ctr.height = 600
-        menu_ctr.width = 1000
-        menu_ctr.content = \
-            ft.Column(controls=[settings_list, bottom_row],
-                      horizontal_alignment=ft.CrossAxisAlignment.CENTER)
-        self.page.add(menu_ctr)
+        return ft.Column(controls=[settings_list, bottom_row], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
